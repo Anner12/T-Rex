63555a4010a63e0bf15747050d55e19bda7910a6
Anne Lie <103604990+Anner12@users.noreply.github.com>
2022-04-27 17:04:34 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 
 var trex, trex_running;
 var edges;
+var ground, groundImg;
 
 function preload(){
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-
+  groundImg = loadImage("ground2.png");
 }
 
 function setup(){
@@ -15,9 +16,11 @@ function setup(){
   trex.addAnimation("running", trex_running);
   edges = createEdgeSprites();
 
-  // alterar tamanho e posição do trex
+  // alterar tamanho e posicao do trex
   trex.scale = 0.5;
   trex.x = 50;
+  ground = createSprite(200, 180, 400, 20);
+  ground.addImage(groundImg); 
 }
 
 function draw(){
@@ -26,9 +29,10 @@ function draw(){
   if (keyDown("space")) {
     trex.velocityY = -10;
   }
+  ground.velocityX = -2;
 
   trex.velocityY = trex.velocityY + 0.5;
-  trex.collide(edges[3]);
+  trex.collide(ground);
 
   drawSprites();
 }
